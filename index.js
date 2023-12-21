eb59b2880bb3f80ff83c4a39b121a59c7219e4cc
Alexey <owsmurf57557@gmail.com>
2023-12-21 12:18:15 +0300

    gaming

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-// @ts-check
+/* eslint-disable import/no-extraneous-dependencies */
+import readlineSync from 'readline-sync';
 
-import half from './src/half.js';
+const choices = ['камень', 'ножницы', 'бумага'];
 
-export default half;
+function getRandomChoice() {
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return 'Ничья!';
+  } if (
+    (userChoice === 'камень' && computerChoice === 'ножницы')
+    || (userChoice === 'ножницы' && computerChoice === 'бумага')
+    || (userChoice === 'бумага' && computerChoice === 'камень')
+  ) {
+    return `Вы победили! ${userChoice} ломает ${computerChoice}.`;
+  }
+  return `Компьютер победил! ${computerChoice} покрывает ${userChoice}.`;
+}
+
+function playGame(round) {
+  if (round > 3) {
+    return;
+  }
+
+  console.log(`Раунд ${round}\n`);
+  console.log('Выберите вашу фигуру:');
+  console.log('1. Камень');
+  console.log('2. Ножницы');
+  console.log('3. Бумага\n');
+
+  const userChoiceIndex = readlineSync.questionInt('Ваш выбор: ');
+  const userChoice = choices[userChoiceIndex - 1];
+
+  console.log(`Вы выбрали: ${userChoice}\n`);
+
+  const computerChoice = getRandomChoice();
+  console.log(`Компьютер выбирает: ${computerChoice}\n`);
+
+  const result = determineWinner(userChoice, computerChoice);
+  console.log(`Результат: ${result}\n`);
+
+  console.log('-------------------------\n');
+
+  playGame(round + 1);
+}
+
+console.log("Добро пожаловать в игру 'Камень, Ножницы, Бумага'!\n");
+playGame(1);
